1883d756d036196049a7620ed93d773c9583ae75
agnesockernahl <agnes.ockernahl@payex.com>
2023-01-24 17:20:48 +0100

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [resturants, setRestaurants] = useState();
+  const fetchData = () => {
+    fetch(
+      "https://hotell.difi.no/api/json/mattilsynet/smilefjes/tilsyn?poststed=trondheim"
+    )
+      .then((response) => response.json())
+      .then((result) => setRestaurants(result.entries));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      {resturants &&
+        resturants.map((restaurant, index) => (
+          <div key={index}>
+            <h1>{restaurant.navn}</h1>
+            <h3>{restaurant.total_karakter}</h3>
+          </div>
+        ))}
     </div>
   );
 }
